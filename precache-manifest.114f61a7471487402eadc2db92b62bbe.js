5a0f3ff34032b6eb34131402f7035b1d94cb509e
Arkadiusz Drukała <arkadiusz.drukala@gmail.com>
2020-08-29 15:20:13 +0200

    Updates

---
+++ precache-manifest.114f61a7471487402eadc2db92b62bbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c8435871e499ce29375a22aa966ee3",
+    "url": "/arkdru23/PokemonApp-in-react/index.html"
+  },
+  {
+    "revision": "11c6fc0eec237c44e0dd",
+    "url": "/arkdru23/PokemonApp-in-react/static/css/main.ec220eb0.chunk.css"
+  },
+  {
+    "revision": "ed1679c8757f9cd22a4b",
+    "url": "/arkdru23/PokemonApp-in-react/static/js/2.c86fa8ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/arkdru23/PokemonApp-in-react/static/js/2.c86fa8ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11c6fc0eec237c44e0dd",
+    "url": "/arkdru23/PokemonApp-in-react/static/js/main.02947628.chunk.js"
+  },
+  {
+    "revision": "c554b3ecb37543f09dd8",
+    "url": "/arkdru23/PokemonApp-in-react/static/js/runtime-main.1e101ada.js"
+  }
+]);
